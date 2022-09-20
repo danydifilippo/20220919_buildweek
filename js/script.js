074124fb58f9e22bd78e22615a3777d9c4b2875d
dimitrilzk <dimitrilazuka95@gmail.com>
2022-09-20 15:23:28 +0200

    da sistemare

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("nav-bar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
