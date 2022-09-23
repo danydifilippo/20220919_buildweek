349b0cfb0e35c64753056288e9fa2b344018534e
dimitrilzk <dimitrilazuka95@gmail.com>
2022-09-23 15:54:05 +0200

    animazione da alto verso basso fatta

--- js/script.js
+++ js/script.js
@@ -17,15 +17,32 @@ window.onscroll = function () {
   }
 };
 
-
-const avantiPagina = function() {
-  const paginaCorrente = document.getElementsByClassName("active")[0]
-  paginaCorrente.classList.remove("active")
-  const numeroPagina = Number(paginaCorrente.id.split("-")[1])
-  const prossimaPagina = document.getElementById("pag-" + (numeroPagina + 1))
-  if(prossimaPagina == null) {
-      document.getElementById("pag-1").classList.add("active")
+const avantiPagina = function () {
+  const paginaCorrente = document.getElementsByClassName("activ")[0];
+  paginaCorrente.classList.remove("activ");
+  const numeroPagina = Number(paginaCorrente.id.split("-")[1]);
+  const prossimaPagina = document.getElementById("pag-" + (numeroPagina + 1));
+  if (prossimaPagina == null) {
+    document.getElementById("pag-1").classList.add("activ");
   } else {
-      prossimaPagina.classList.add("active")
+    prossimaPagina.classList.add("activ");
+  }
+};
+
+function fadeinup() {
+  let fadeinup = document.querySelectorAll(".fadeinup");
+
+  for (let i = 0; i < fadeinup.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = fadeinup[i].getBoundingClientRect().top;
+    let elementVisible = 0;
+
+    if (elementTop < windowHeight - elementVisible) {
+      fadeinup[i].classList.add("active");
+    } else {
+      fadeinup[i].classList.remove("active");
+    }
   }
 }
+
+window.addEventListener("scroll", fadeinup);
